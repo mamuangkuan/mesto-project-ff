77374dc0039910ec4b0dde0c8888bdf5fc636e2b
Evgenii Iakovina <evpen@yandex.ru>
2024-06-26 17:56:58 +0300

    версия 2.2 (внесены косметические правки)

--- src/components/card.js
+++ src/components/card.js
@@ -5,24 +5,18 @@ const cardTemplate = document.querySelector("#card-template").content;
 export function createNewCard(item, removeCard, bigImgModal, likeCard) {
   // Клонирование экземпляра карточки
   const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true);
-
   // Кнопка удаления карточки
   const removeButton = cardItem.querySelector(".card__delete-button");
-
   // Кнопка лайка
-  const likeButton = cardItem.querySelector('.card__like-button')
-
+  const likeButton = cardItem.querySelector(".card__like-button");
   // Блок с картинкой
   const cardImageUrl = cardItem.querySelector(".card__image");
-
   // Заголовок картинки
   const cardTitle = cardItem.querySelector(".card__title");
-
   // Заполняем переменные данными
   cardImageUrl.src = item.link;
   cardTitle.alt = item.name;
   cardTitle.textContent = item.name;
-  
   // Обработчик открытия большой картинки
   cardImageUrl.addEventListener("click", function () {
     bigImgModal(item);
@@ -32,20 +26,20 @@ export function createNewCard(item, removeCard, bigImgModal, likeCard) {
   removeButton.addEventListener("click", removeCard);
 
   // Обработчик кнопки лайка
-  likeButton.addEventListener('click', likeCard);
+  likeButton.addEventListener("click", likeCard);
 
   // Возврат готового результата
   return cardItem;
-};
+}
 
 // Функция удаления карточки
 export function removeCard(evt) {
   evt.target.closest(".places__item").remove();
-};
+}
 
 // Функция лайка карточки
-export function likeCard (evt) {
+export function likeCard(evt) {
   if (evt.target.classList.contains("card__like-button")) {
     evt.target.classList.toggle("card__like-button_is-active");
-   }
-};
+  }
+}
